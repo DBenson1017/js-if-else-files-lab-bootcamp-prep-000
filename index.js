71b6549ab886691833d71faec4d6a00abfe18cbb
Devin Benson <devinbenson@gmail.com>
2019-09-15 15:33:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var name = "Joe"
 if (name === 'Susan') {
   console.log('The name is correct')
-} else (name === 'Susan') {
-  console.log()
+} else {
+  console.log('Expected the name to be Susan')
 }
-
 
 var height = "74"
 
